554b2b4e94d698915a6ddfdd6fce7e871e4a0dc6
xugr <guangrong_xu@126.com>
2017-02-19 22:02:48 +0800

    初始化上传
    Signed-off-by: xugr <guangrong_xu@126.com>

---
+++ test-quartz/src/main/java/com/redsun/quartz/bean/BeanA.java
@@ -0,0 +1,21 @@
+package com.redsun.quartz.bean;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+/**
+ * Created by xugr on 2017/2/17.
+ */
+
+@Slf4j
+@Service
+public class BeanA {
+
+    public void work () {
+        Date date = new Date();
+        log.info("-----------------------{}", date);
+    }
+
+}
