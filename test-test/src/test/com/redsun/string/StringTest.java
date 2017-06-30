fb76fb704f49488ec18a0692bc60cfe69bada358
xugr <xugr>
2017-06-30 09:51:43 +0800

    dubbo 测试

--- test-test/src/test/com/redsun/string/StringTest.java
+++ test-test/src/test/com/redsun/string/StringTest.java
@@ -14,4 +14,36 @@ public class StringTest {
         StringBuffer sb = new StringBuffer();
         log.info("------reslt = {}", "".equals(sb.toString()));
     }
+
+    @Test
+    public void testSwitch() {
+        switch ("xugr") {
+            case "1xugr1" :
+                log.info("xugr1 xugr1");
+                break;
+            case "xugr" :
+                log.info("xugr xugr");
+                break;
+            case "1xugr2" :
+                log.info("xugr2 xugr2");
+                break;
+            default:
+                log.info("default");
+                break;
+        }
+    }
+
+    @Test
+    public void testSwitchNumBer() {
+        switch (2) {
+            case 1 :
+                log.info("xugr xugr");
+            case 2 :
+                log.info("xugr1 xugr1");
+            case 4 :
+                log.info("xugr2 xugr2");
+            default:
+                log.info("default");
+        }
+    }
 }
