1b9ab56f04cbd884faf0c09c41b9d25c137b0c29
xugr <xugr>
2017-07-20 16:51:41 +0800

    upload test case

--- test-test/src/test/java/com/redsun/date/DateTest.java
+++ test-test/src/test/java/com/redsun/date/DateTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -16,6 +18,15 @@ public class DateTest {
     public void testAddDay() {
         Date date = DateUtils.addDays(new Date(), 1);
         log.info("------date = {}", date);
+    }
+
+    public void testConvertDateFormat () throws ParseException {
+        String strDate = "2017.07.19 19:53:22";
+        String format1 = "yyyy.MM.dd HH:mm:ss";
+
+        Date date = DateUtils.parseDate(strDate, format1);
+        log.info("------------date = {}", date);
+
     }
 
 }
