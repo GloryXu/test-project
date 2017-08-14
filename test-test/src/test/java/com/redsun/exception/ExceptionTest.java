bd8f0b8ae0fbabb85cd93ae1def1f42e17c74eb4
xugr <xugr>
2017-08-14 16:55:36 +0800

    Upload test file about Exception .
    Signed-off-by: xugr <xugr>

---
+++ test-test/src/test/java/com/redsun/exception/ExceptionTest.java
@@ -0,0 +1,29 @@
+package com.redsun.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class ExceptionTest {
+
+    @Test
+    public void testTryCatch() {
+        log.info("--------------result = {}", method());
+    }
+
+    private int method() {
+        try {
+            log.info("try");
+
+            // This returning code will be not executed.
+            return 1;
+        } catch (Exception e) {
+            log.info("catch");
+            return 2;
+        } finally {
+            log.info("finally");
+            return 3;
+        }
+    }
+
+}
