426dca5cb2c2cf7f0ce9bb21562966c3d441c5af
xugr <xugr>
2017-09-25 14:28:54 +0800

    反射测试代码上传

---
+++ test-test/src/test/java/test/redsun/reflect/ReflectTest.java
@@ -0,0 +1,36 @@
+package test.redsun.reflect;
+
+import java.lang.reflect.Modifier;
+
+import org.junit.Test;
+
+public class ReflectTest {
+
+	@Test
+	public void testA(){
+		int a = 100;
+		int b = 200;
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+		System.out.println("a = " + a + ", b = " + b);
+	}
+	
+	@Test
+	public void testModifier(){
+		System.out.println(Modifier.FINAL);
+		System.out.println(Modifier.isFinal(String.class.getModifiers()));
+		System.out.println(Modifier.isFinal(Integer.class.getModifiers()));
+		System.out.println(Modifier.isInterface(Integer.class.getModifiers()));
+	}
+	
+	@Test
+	public void testDataTransform(){
+		int a = 1073741824;
+		System.out.println(Integer.toHexString(a));
+		a = 48;
+		System.out.println(Integer.toHexString(a));
+//		System.out.println(Integer.parseInt("01000000000000000000000000000000",2));
+//		System.out.println(Integer.numberOfLeadingZeros(a));
+	}
+}
