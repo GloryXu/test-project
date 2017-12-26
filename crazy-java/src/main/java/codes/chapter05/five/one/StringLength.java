303fe3d1771662b9146dd3deca57619da58b2c2f
xugr <xugr>
2017-12-26 21:57:20 +0800

    泛型可能引起的错误，原始泛型变量的复制，原始类型带来的擦除

---
+++ crazy-java/src/main/java/codes/chapter05/five/one/StringLength.java
@@ -0,0 +1,8 @@
+package codes.chapter05.five.one;
+
+public class StringLength {
+    public static void main(String[] args) {
+        // 编译出错
+//        System.out.println("abc""\ u000a".length());
+    }
+}
