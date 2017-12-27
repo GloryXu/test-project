88bfb91e32235fcf982650bb13c426a1b9901a31
xugr <xugr>
2017-12-27 17:10:02 +0800

    instanceof运算符的陷阱

---
+++ crazy-java/src/main/java/codes/chapter07/two/one/Cat.java
@@ -0,0 +1,25 @@
+package codes.chapter07.two.one;
+
+public class Cat {
+    private String name;
+    private double weight;
+    private String color;
+
+    /**
+     * 添加void之后，不再是构造器，而是普通方法
+     */
+    public void Cat() {
+        this.name = "Garfield";
+        this.weight = 20;
+        this.color = "orange";
+    }
+    public void say () {
+        System.out.println("体重：" + weight + ",毛色：" + color);
+        System.out.println(name + "要有了三个愿望：'第一个是要猪肉卷，" + "第二个还是猪肉卷，第三个，哦，你错啦，" + "我想要更多的愿望，那样我就能得到更多的猪肉卷啦。'");
+    }
+
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.say();
+    }
+}
