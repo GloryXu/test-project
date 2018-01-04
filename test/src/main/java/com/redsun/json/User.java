1a7cd4c11ea36a4c7a279a932a30249bb2649d59
xugr <xugr>
2018-01-04 16:49:00 +0800

    添加Gson实现

---
+++ test/src/main/java/com/redsun/json/User.java
@@ -0,0 +1,16 @@
+package com.redsun.json;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+
+    private int age;
+
+    private String password;
+}
