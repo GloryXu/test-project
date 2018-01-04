64ae3feb04e78bc78041bd598aa5b5d7b93a5416
xugr <xugr>
2018-01-04 21:10:16 +0800

    哈夫曼树、红黑树，排序二叉树的概念了解

---
+++ test/src/main/java/com/redsun/queue/QueueDemo.java
@@ -0,0 +1,22 @@
+package com.redsun.queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueDemo {
+
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayDeque<>(2);
+        System.out.println("---" + queue);
+        queue.offer("aaa");
+        queue.offer("bbb");
+        queue.offer("ccc");
+        System.out.println("---" + queue);
+        queue.add("ddd");
+        System.out.println("---" + queue);
+        System.out.println(queue.peek());
+        System.out.println("after peek " + queue);
+        System.out.println(queue.poll());
+        System.out.println("after poll " + queue);
+    }
+}
