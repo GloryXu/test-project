b83ab929c9f50ed249507d17eb2d0c1a32ba1a13
xugr <xugr>
2018-03-09 16:51:48 +0800

    上传Boolean和Double的测试类，测试Byte,Short,Integer,Long,Character中的常量池缓存，

---
+++ test/src/main/java/pattern/BooleanTest.java
@@ -0,0 +1,9 @@
+package pattern;
+
+public class BooleanTest {
+    public static void main(String[] args) {
+        Boolean b1 = true;
+        Boolean b2 = true;
+        System.out.println(b1 == b2);
+    }
+}
---
+++ test/src/main/java/pattern/DoubleTest.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class DoubleTest {
+    public static void main(String[] args) {
+        Double d1 = 1.0;
+        Double d2 = 1.0;
+        System.out.println(d1 == d2);
+
+        Float f1 = 1.0f;
+        Float f2 = 1.0f;
+        System.out.println(f1 == f2);
+    }
+}
