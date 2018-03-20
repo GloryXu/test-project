57fb90657a0a34082d35b05f6b9117da297286ca
xugr <xugr>
2018-03-20 15:19:40 +0800

    finalize 功能以及使用

---
+++ test/src/main/java/jvm/garbage/collection/CanReliveObj.java
@@ -0,0 +1,43 @@
+package jvm.garbage.collection;
+
+public class CanReliveObj {
+    public static CanReliveObj obj;
+
+    /**
+     * finalize只能调用一次
+     *
+     * @throws Throwable
+     */
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("CanReliveObj finalize called");
+        obj = this;
+    }
+
+    @Override
+    public String toString() {
+        return "I am CanReliveObj";
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        obj = new CanReliveObj();
+        obj = null; // 可复活
+        System.gc();
+        Thread.sleep(1000);
+        if (obj == null) {
+            System.out.println("obj 是 null");
+        } else {
+            System.out.println("obj 可用");
+        }
+        System.out.println("第二次gc");
+        obj = null; // 不可复活
+        System.gc();
+        Thread.sleep(1000);
+        if (obj == null) {
+            System.out.println("obj 是 null");
+        } else {
+            System.out.println("obj 可用");
+        }
+    }
+}
