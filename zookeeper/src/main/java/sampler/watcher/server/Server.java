ed5fadcf45cebe62d8ef5beed3ae3fa320fd0bd2
glory <guangrong_xu@126.com>
2018-12-17 23:54:28 +0800

    client demo

---
+++ zookeeper/src/main/java/sampler/watcher/server/Server.java
@@ -0,0 +1,30 @@
+package sampler.watcher.server;
+
+import org.apache.zookeeper.server.NIOServerCnxnFactory;
+import org.apache.zookeeper.server.ZooKeeperServer;
+import org.apache.zookeeper.server.persistence.FileTxnSnapLog;
+import sampler.watcher.Conf;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * @author glory
+ * @description zk server端
+ * @date Created at 下午11:35 18-12-17
+ */
+public class Server {
+    NIOServerCnxnFactory serverCnxnFactory;
+
+    public void zkStart(Conf conf) throws IOException, InterruptedException {
+        ZooKeeperServer zkServer = new ZooKeeperServer();
+        File dataDirectory = new File(conf.DIR);
+        FileTxnSnapLog ftxn = new FileTxnSnapLog(dataDirectory, dataDirectory);
+        zkServer.setTxnLogFactory(ftxn);
+        zkServer.setTickTime(conf.TICK_TIME);
+        serverCnxnFactory = new NIOServerCnxnFactory();
+        serverCnxnFactory.configure(new InetSocketAddress(conf.PORT), conf.MAX_CLIENT_CONNECTIONS);
+        serverCnxnFactory.startup(zkServer);
+    }
+}
---
+++ zookeeper/src/main/java/sampler/watcher/server/ZkServer.java
@@ -0,0 +1,28 @@
+package sampler.watcher.server;
+
+import sampler.watcher.Conf;
+
+import java.io.IOException;
+
+/**
+ * @author glory
+ * @description zkServer启动类
+ * @date Created at 下午11:38 18-12-17
+ */
+public class ZkServer {
+    public static void main(String[] args) {
+        Conf conf = new Conf();
+        conf.DIR = "/zookeeper";
+        conf.PORT = 22801;
+        conf.TICK_TIME = 2000;
+        conf.MAX_CLIENT_CONNECTIONS = 60;
+        Server srv = new Server();
+        try {
+            srv.zkStart(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
