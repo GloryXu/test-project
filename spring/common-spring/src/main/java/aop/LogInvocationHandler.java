d91364b1995be43f865d8f64c560cfa5bd2436e5
glory <guangrong_xu@126.com>
2019-01-14 22:54:30 +0800

    modify log info

--- spring/common-spring/src/main/java/aop/LogInvocationHandler.java
+++ spring/common-spring/src/main/java/aop/LogInvocationHandler.java
@@ -15,10 +15,10 @@ public class LogInvocationHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         // 执行织入的日志，你可以控制哪些方法执行切入逻辑
         if (method.getName().equals("doSomeThing2")) {
-            System.out.println("记录日志");
+            System.out.println("业务逻辑2------记录日志");
         }
         if (method.getName().equals("doSomeThing")) {
-            System.out.println("doSomeThing------记录日志");
+            System.out.println("业务逻辑------记录日志");
         }
         // 执行原有逻辑
         Object recv = method.invoke(target, args);
