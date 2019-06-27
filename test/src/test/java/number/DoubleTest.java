4c619d047be18ec83f1eda7c48bc5a23f08de89b
xuguangrong <xuguangrong@jd.com>
2019-06-27 15:10:55 +0800

    double  测试类
    Signed-off-by: xuguangrong <xuguangrong@jd.com>

--- test/src/test/java/number/DoubleTest.java
+++ test/src/test/java/number/DoubleTest.java
@@ -1,8 +1,11 @@
 package number;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +18,7 @@ public class DoubleTest {
 
     @Test
     public void testConvertJSON() {
-        Double d = 0.000004d;
+        Double d = 0.00000004d;
 
         Map<String, Object> map = new HashMap<>();
 
@@ -24,5 +27,12 @@ public class DoubleTest {
         System.out.println(map.toString());
 
         System.out.println(JSONObject.toJSON(map));
+
+        DecimalFormat df = new DecimalFormat("0.00000000");
+        System.out.println(df.format(((JSONObject) JSONObject.toJSON(map)).getDouble("double")));
+
+        System.out.println(BigDecimal.valueOf((Double) map.get("double")).toPlainString());
+        System.out.println(BigDecimal.valueOf((Double) map.get("double")).toString());
+        System.out.println(JSON.toJSONString(map));
     }
 }
