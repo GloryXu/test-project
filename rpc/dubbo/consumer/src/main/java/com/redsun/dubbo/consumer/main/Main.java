dd5250c773542407bd7223608f189f5f86b18e6d
xuguangrong <xuguangrong@jd.com>
2019-07-14 17:20:29 +0800

    dubbo 升级至 2.7.2版本
    Signed-off-by: xuguangrong <xuguangrong@jd.com>

--- rpc/dubbo/consumer/src/main/java/com/redsun/dubbo/consumer/main/Main.java
+++ rpc/dubbo/consumer/src/main/java/com/redsun/dubbo/consumer/main/Main.java
@@ -5,15 +5,19 @@ import com.redsun.dubbo.provider.ProviderI;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+
 /**
  * Created by xugr on 2017/3/22.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/spring-context.xml");
         ProviderI providerI = (ProviderI)context.getBean("providerI");
         Consumer consumer = (Consumer)context.getBean("consumer");
         consumer.testProvider();
+
+        System.in.read();
     }
 }
