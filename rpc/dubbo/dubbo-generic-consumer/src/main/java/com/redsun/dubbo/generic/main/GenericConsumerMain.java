6f92fab172b42c80a0eae40e9609995b46c917d0
xuguangrong <xuguangrong@jd.com>
2019-09-08 23:59:42 +0800

    dubbo 泛化调用demo实现
    Signed-off-by: xuguangrong <xuguangrong@jd.com>

--- rpc/dubbo/dubbo-generic-consumer/src/main/java/com/redsun/dubbo/generic/main/GenericConsumerMain.java
+++ rpc/dubbo/dubbo-generic-consumer/src/main/java/com/redsun/dubbo/generic/main/GenericConsumerMain.java
@@ -1,11 +1,10 @@
 package com.redsun.dubbo.generic.main;
 
-import com.redsun.dubbo.generic.provider.DemoServiceImpl;
-import com.redsun.dubbo.provider.DemoService;
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.ConsumerConfig;
 import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.rpc.service.GenericService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,36 +13,44 @@ import java.io.IOException;
 /**
  * @author xuguangrong
  * @description 泛化消费者执行类
- * 依赖接口
+ * 不依赖接口
  * @date Created at 21:59 2019/7/14
  */
 public class GenericConsumerMain {
     private static final Logger logger = LoggerFactory.getLogger(GenericConsumerMain.class);
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
+    public static void main(String[] args) throws InterruptedException, IOException {
+        ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
         ApplicationConfig applicationConfig = new ApplicationConfig("dubbo-demo-generic-consumer");
         applicationConfig.setQosPort(22221);// 默认22222
         reference.setApplication(applicationConfig);
         reference.setRegistry(new RegistryConfig("zookeeper://127.0.0.1:2181"));
-        reference.setInterface(DemoService.class);
+        reference.setInterface("com.redsun.dubbo.provider.DemoService");
         reference.setGroup("providerGroup");// 需要和服务提供者一致，否则报错
-//        boolean isLazy = false;
+        reference.setGeneric("true");
+//        boolean isLazy = false;// 默认false
         boolean isLazy = true;
         reference.setLazy(isLazy);
 
         ConsumerConfig consumerConfig = new ConsumerConfig();
-//        consumerConfig.setLazy(isLazy);
         consumerConfig.setCorethreads(2);
         consumerConfig.setGroup("consumerGroup");
         reference.setConsumer(consumerConfig);
 
-        DemoService service = reference.get();
-        Thread.sleep(3000);
-        long startTime = System.currentTimeMillis();
+        GenericService service = reference.get();
+        if (!isLazy) {
+            // 不延时
+            Thread.sleep(1000);
+        }
 
-        String message = service.sayHello("dubbo");
-        logger.info((isLazy ? "延时":"非延时") + "加载响应耗时：" + (System.currentTimeMillis() - startTime) + "ms, 响应信息：" + message);
+        long startTime = System.currentTimeMillis();
+        Object response = service.$invoke("sayHello", new String[]{String.class.getName()}, new Object[]{"dubbo"});
+        logger.info((isLazy ? "延时":"非延时") + "加载响应耗时：" + (System.currentTimeMillis() - startTime) + "ms, 响应信息：" + response);
+
+        startTime = System.currentTimeMillis();
+        Object response1 = service.$invoke("sayHello", new String[]{String.class.getName()}, new Object[]{"第二次调用"});
+        logger.info("第二次调用耗时" + (System.currentTimeMillis() - startTime));
+        logger.info("response type is = " + response1.getClass().getName());
 
         System.in.read();
     }
--- rpc/dubbo/dubbo-generic/src/main/java/com/redsun/dubbo/generic/main/GenericProviderMain.java
+++ rpc/dubbo/dubbo-generic/src/main/java/com/redsun/dubbo/generic/main/GenericProviderMain.java
@@ -3,6 +3,7 @@ package com.redsun.dubbo.generic.main;
 import com.redsun.dubbo.generic.provider.DemoServiceImpl;
 import com.redsun.dubbo.provider.DemoService;
 import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.MethodConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.config.ServiceConfig;
 
@@ -21,6 +22,8 @@ public class GenericProviderMain {
         service.setRegistry(new RegistryConfig("zookeeper://127.0.0.1:2181"));
         service.setInterface(DemoService.class);
         service.setRef(new DemoServiceImpl());
+        service.setGroup("providerGroup");
+        service.setGeneric("true");
         service.export();
 
         System.in.read();
